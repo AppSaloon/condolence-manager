2e1e567b7c88a8c65c413db98cbf85f0dfd1760a
Aytaç Kokus <aytackokus@MBP13-Reserve-698.local>
2016-02-19 13:56:46 +0100

    mail to family and person

--- includes/controller/templates.php
+++ includes/controller/templates.php
@@ -7,6 +7,7 @@ class Templates{
     public function __construct()
     {
         add_filter( 'template_include', array($this, 'cm_template_chooser') );
+        add_action( 'comment_post', array($this, 'send_comment_email_notification'), 11, 2 );
     }
 
     public function cm_template_chooser( $template ) {
@@ -49,5 +50,30 @@ class Templates{
         wp_enqueue_script('jquery');
         wp_enqueue_style('template-single', CM_URL . 'css/template-single.css');
         wp_enqueue_script('template-single', CM_URL . 'js/template-single.js');
+    }
+
+    public function send_comment_email_notification( $comment_ID, $commentdata ) {
+            $comment = get_comment( $comment_ID );
+            $postid = $comment->comment_post_ID;
+            $master_email = get_post_meta( $postid, 'email', true);
+
+            // mail to family
+            if( isset( $master_email ) && is_email( $master_email ) && $comment->parent == 0) {
+                $message = 'New <a href="' . get_permalink( $postid ) . '">condolence</a><br />';
+                $message .= 'From: '.$comment->comment_author;
+                $message .= '<p>'.nl2br($comment->comment_content).'</p>';
+                add_filter( 'wp_mail_content_type', create_function( '', 'return "text/html";' ) );
+                wp_mail( $master_email, 'New Comment', $message );
+            }
+
+            // mail to person
+            if( isset( $master_email ) && is_email( $master_email ) && $comment->parent != 0) {
+                $parent_comment = get_comment( $comment->parent );
+                $message = 'New <a href="' . get_permalink( $postid ) . '">condolence</a><br />';
+                $message .= 'From: '.$comment->comment_author;
+                $message .= '<p>'.nl2br($comment->comment_content).'</p>';
+                add_filter( 'wp_mail_content_type', create_function( '', 'return "text/html";' ) );
+                wp_mail( $parent_comment->comment_author_email, 'New Comment', $message );
+            }
     }
 }
