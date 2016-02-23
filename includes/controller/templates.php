b3243562e23fc85f1bcc6aa1fcf264c70b0f9d9e
miekenijs <mieke@appsaloon.be>
2016-02-23 09:13:19 +0100

    template mail + css single post

--- includes/controller/templates.php
+++ includes/controller/templates.php
@@ -59,19 +59,19 @@ class Templates{
 
             // mail to family
             if( isset( $master_email ) && is_email( $master_email ) && $comment->parent == 0) {
-                $message = 'New <a href="' . get_permalink( $postid ) . '">condolence</a><br />';
-                $message .= 'From: '.$comment->comment_author;
-                $message .= '<p>'.nl2br($comment->comment_content).'</p>';
+                $message = '<p style="font-size: 22px; font-weight: bold; line-height: 26px; vertical-align: 20px; margin-top: 50px;">New <a href="' . get_permalink( $postid ) . '/?password='.get_post_meta($postid, 'password', true).'">condolence</a>&nbsp;';
+                $message .= 'from '.$comment->comment_author. ':</p>';
+                $message .= '<p style="font-size: 16px; font-weight: normal; margin: 16px 0;">'.nl2br($comment->comment_content).'</p>';
                 add_filter( 'wp_mail_content_type', create_function( '', 'return "text/html";' ) );
-                wp_mail( $master_email, 'New Comment', $message );
+                wp_mail( $master_email, 'New Condolence', $message );
             }
 
             // mail to person
             if( isset( $master_email ) && is_email( $master_email ) && $comment->parent != 0) {
                 $parent_comment = get_comment( $comment->parent );
-                $message = 'New <a href="' . get_permalink( $postid ) . '">condolence</a><br />';
-                $message .= 'From: '.$comment->comment_author;
-                $message .= '<p>'.nl2br($comment->comment_content).'</p>';
+                $message = '<p style="font-size: 22px; font-weight: bold; line-height: 26px; vertical-align: 20px; margin-top: 50px;">New <a href="' . get_permalink( $postid ) . '">reply</a>&nbsp;';
+                $message .= 'on your condolence from '.$comment->comment_author.':</p>';
+                $message .= '<p style="font-size: 16px; font-weight: normal; margin: 16px 0;">'.nl2br($comment->comment_content).'</p>';
                 add_filter( 'wp_mail_content_type', create_function( '', 'return "text/html";' ) );
                 wp_mail( $parent_comment->comment_author_email, 'New Comment', $message );
             }
