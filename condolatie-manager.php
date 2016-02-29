b004a11772754abfa50b998da23b302e9d4a242a
miekenijs <mieke@appsaloon.be>
2016-02-29 12:49:19 +0100

    add license

--- condolatie-manager.php
+++ condolatie-manager.php
@@ -2,13 +2,31 @@
 /**
  * Plugin Name: Condolence Manager
  * Plugin URI: http://www.appsaloon.be
- * Description: Deze plugin laat bezoekers toe om de familie van overledenen te condoleren.
+ * Description: This plugin allows visitors to condole the family of the deceased.
  * Version: 1.0
  * Text Domain: cm_translate
  * Author: AppSaloon
  * Author URI: http://www.appsaloon.be
  * License: GPLv2 or later
  */
+
+/*  Copyright 2014	AppSaloon  (email : info@appsaloon.be)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 define( 'CM_BASE_FILE', __FILE__ );
 define( 'CM_BASE_DIR', dirname( CM_BASE_FILE ) );
 define( 'CM_URL', plugin_dir_url( __FILE__ ));
