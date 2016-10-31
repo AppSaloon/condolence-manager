8c86c9cee811f53389dbcf2c53f6796ead8da759
Sebiastian <sebastian@appsaloon.be>
2016-10-31 16:15:37 +0100

    metadadata update

--- includes/koffie-tafel/koffie-tafel-controller.php
+++ includes/koffie-tafel/koffie-tafel-controller.php
@@ -9,13 +9,27 @@ class Koffie_Tafel_Controller
 
     }
 
-    function add_button($content)
+    function gf_data_saver($content)
     {
 
-		var_dump($content);die;
+		if( $content ){
+			$name = trim(sanitize_text_field($content[1]));
+			$surname = trim(sanitize_text_field($content[2]));
+			$email = trim(sanitize_text_field($content[3]));
+			$gsm = trim(sanitize_text_field($content[4]));
+			$post_id = trim(sanitize_text_field($content[5]));
+
+
+			$tmp_string = $name . "-" . $surname . "-" . $email . "-" . $gsm;
+
+				$meta_key = '_koffie_tafel_'.time();
+				update_metadata('post', $post_id, $meta_key, $tmp_string);
+				unset ($tmp_string);
+		}
+
 
         return $content;
     }
-
+	
 
 }
