f4db93569eba499445e88b009aa6ac0d20aa8bfd
david <david@appsaloon.be>
2020-05-15 15:19:39 +0200

    refact(Comment_Email): prepare sql query

--- src/controller/Comment_Email.php
+++ src/controller/Comment_Email.php
@@ -87,7 +87,7 @@ class Comment_Email
     {
         global $wpdb;
 
-        $query = "UPDATE " . $wpdb->prefix . "comments SET comment_approved='1' WHERE comment_ID=" . $comment_ID;
+        $query = $wpdb->prepare("UPDATE " . $wpdb->prefix . "comments SET comment_approved='1' WHERE comment_ID=%d", $comment_ID);
         $wpdb->query($query);
 
         clean_comment_cache($comment_ID);
