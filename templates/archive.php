d2b7e5f2a681435723ec82e637d99294ae044a8d
david <david@appsaloon.be>
2020-05-15 11:07:44 +0200

    fix(archive): escape output

--- templates/archive.php
+++ templates/archive.php
@@ -15,7 +15,7 @@ get_header(); ?>
                 <div class="rouw entry-content clear">
                     <article>
                         <div class="embed clear">
-                            <div class="deceased-img" <?php echo 'style="min-height: 252px;"' ?>>
+                            <div class="deceased-img" style="min-height: 252px;">
 								<?php if ( has_post_thumbnail() ) : ?>
                                     <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 										<?php the_post_thumbnail( 'medium' ); ?>
@@ -24,13 +24,13 @@ get_header(); ?>
                             </div>
                             <div class="deceased-info">
                                 <div class="deceased-name">
-									<?php echo $post_meta["name"][0] . '&nbsp;' . $post_meta["familyname"][0]; ?>
+									<?php echo esc_html( $post_meta["name"][0] ) . '&nbsp;' . esc_html( $post_meta["familyname"][0] ); ?>
                                 </div>
 								<?php
 								if ( isset( $post_meta["residence"][0] ) && $post_meta["residence"][0] ) { ?>
                                     <div class="deceased-subtitle">
-										<?php _e( 'Resident of', 'cm_translate' ); ?>
-                                        : <?php echo $post_meta["residence"][0]; ?>
+										<?php echo esc_html__( 'Resident of', 'cm_translate' ); ?>
+                                        : <?php echo esc_html( $post_meta["residence"][0] ); ?>
                                     </div>
 									<?php
 								}
@@ -45,69 +45,58 @@ get_header(); ?>
 										foreach ( $relations as $relation ) {
 											if ( $relation['type'] == 'Married' && $relation['alive'] == '1' && $gender == 'Male' ) {
 												echo '<p class="alive">';
-												_e( 'Beloved husband of', 'cm_translate' );
+												echo esc_html__( 'Beloved husband of', 'cm_translate' );
 												echo ':&nbsp;';
-												echo $relation['name'] . '&nbsp;' . $relation['familyname'];
+												echo esc_html( $relation['name'] ) . '&nbsp;' . esc_html( $relation['familyname'] );
 												echo '</p>';
 											} elseif ( $relation['type'] == 'Married' && $relation['alive'] == '1' && $gender == 'Female' ) {
 												echo '<p class="alive">';
-												_e( 'Beloved wife of', 'cm_translate' );
+												echo esc_html__( 'Beloved wife of', 'cm_translate' );
 												echo ':&nbsp;';
-												echo $relation['name'] . '&nbsp;' . $relation['familyname'];
+												echo esc_html( $relation['name'] ) . '&nbsp;' . esc_html( $relation['familyname'] );
 												echo '</p>';
 											} elseif ( $relation['type'] == 'Married' && $relation['alive'] == '0' && $gender == 'Male' ) {
 												echo '<p class="alive">';
-												_e( 'Beloved husband of the late', 'cm_translate' );
+												echo esc_html__( 'Beloved husband of the late', 'cm_translate' );
 												echo ':&nbsp;';
-												echo $relation['name'] . '&nbsp;' . $relation['familyname'];
+												echo esc_html( $relation['name'] ) . '&nbsp;' . esc_html( $relation['familyname'] );
 												echo '</p>';
 											} elseif ( $relation['type'] == 'Married' && $relation['alive'] == '0' && $gender == 'Female' ) {
 												echo '<p class="alive">';
-												_e( 'Beloved wife of the late', 'cm_translate' );
+												echo esc_html__( 'Beloved wife of the late', 'cm_translate' );
 												echo ':&nbsp;';
-												echo $relation['name'] . '&nbsp;' . $relation['familyname'];
+												echo esc_html( $relation['name'] ) . '&nbsp;' . esc_html( $relation['familyname'] );
 												echo '</p>';
-											} elseif ( $relation['type'] == 'Other' && $relation['alive'] == '1' && $gender == 'Male' ) {
+											} elseif ( $relation['type'] == 'Other' ) {
 												echo '<p class="alive">';
-												echo $relation['other'] . '&nbsp;' . $relation['name'] . '&nbsp;' . $relation['familyname'];
-												echo '</p>';
-											} elseif ( $relation['type'] == 'Other' && $relation['alive'] == '1' && $gender == 'Female' ) {
-												echo '<p class="alive">';
-												echo $relation['other'] . '&nbsp;' . $relation['name'] . '&nbsp;' . $relation['familyname'];
-												echo '</p>';
-											} elseif ( $relation['type'] == 'Other' && $relation['alive'] == '0' && $gender == 'Male' ) {
-												echo '<p class="alive">';
-												echo $relation['other'] . '&nbsp;' . $relation['name'] . '&nbsp;' . $relation['familyname'];
-												echo '</p>';
-											} elseif ( $relation['type'] == 'Other' && $relation['alive'] == '0' && $gender == 'Female' ) {
-												echo '<p class="alive">';
-												echo $relation['other'] . '&nbsp;' . $relation['name'] . '&nbsp;' . $relation['familyname'];
+												echo esc_html( $relation['other'] ) . '&nbsp;' . esc_html( $relation['name'] ) . '&nbsp;' . esc_html( $relation['familyname'] );
 												echo '</p>';
 											}
 										}
 									}
 									?></div>
                                 <div class="deceased-partner">
-									<?php _e( 'Born', 'cm_translate' ); ?>
-									<?php _e( 'in', 'cm_translate' ); ?>:
-									<?php echo $post_meta["birthplace"][0]; ?>
+									<?php echo esc_html__( 'Born', 'cm_translate' ); ?>
+									<?php echo esc_html__( 'in', 'cm_translate' ); ?>:
+									<?php echo esc_html( $post_meta["birthplace"][0] ); ?>
 									<?php if ( isset( $post_meta["birthdate"][0] ) && $post_meta["birthdate"][0] != '' ) {
-										echo __( 'on', 'cm_translate' ) . ':&nbsp;';
+										echo esc_html__( 'on', 'cm_translate' ) . ':&nbsp;';
 									}
 									$date = $post_meta["birthdate"][0];
-									echo $date;
+									echo esc_html( $date );
 									?>
                                 </div>
                                 <div class="deceased-place-died">
-									<?php _e( 'Passed away', 'cm_translate' ); ?>
-									<?php _e( 'in', 'cm_translate' ); ?>:
-									<?php echo $post_meta["placeofdeath"][0]; ?>
+									<?php echo esc_html__( 'Passed away', 'cm_translate' ); ?>
+									<?php echo esc_html__( 'in', 'cm_translate' ); ?>:
+									<?php echo esc_html( $post_meta["placeofdeath"][0] ); ?>
 									<?php if ( isset( $post_meta["dateofdeath"][0] ) && $post_meta["dateofdeath"][0] != '' ) {
-										echo __( 'on', 'cm_translate' ) . ':&nbsp;';
+										echo esc_html__( 'on', 'cm_translate' ) . ':&nbsp;';
 									}
 									$date = $post_meta["dateofdeath"][0];
-									echo $date;
-									?></div>
+									echo esc_html( $date );
+									?>
+                                </div>
 								<?php
 								if ( isset( $post_meta["_cm_linked_location"] ) ) {
 									$location_id = current( $post_meta["_cm_linked_location"] );
@@ -115,8 +104,8 @@ get_header(); ?>
 										$location = get_the_title( $location_id );
 										if ( ! empty( $location ) ) { ?>
                                             <div class="_cm_linked_location">
-												<?php _e( "Laid out at", "cm_translate" ); ?>:
-												<?php echo $location; ?>
+												<?php echo esc_html__( "Laid out at", "cm_translate" ); ?>:
+												<?php echo esc_html( $location ); ?>
                                             </div>
 											<?php
 										}
@@ -125,24 +114,26 @@ get_header(); ?>
 								?>
 								<?php if ( $post_meta["funeralinformation"][0] ) { ?>
                                     <div class="deceased-uitvaart">
-										<?php _e( 'Funeral information', 'cm_translate' ); ?>:
-										<?php echo $post_meta["funeralinformation"][0]; ?>
+										<?php echo esc_html__( 'Funeral information', 'cm_translate' ); ?>:
+										<?php echo esc_html( $post_meta["funeralinformation"][0] ); ?>
                                     </div>
 								<?php } ?>
 								<?php if ( $post_meta["prayervigilinformation"][0] ) { ?>
                                     <div class="deceased-wake">
-										<?php _e( 'Prayer vigil information', 'cm_translate' ); ?>:
-										<?php echo $post_meta["prayervigilinformation"][0]; ?>
+										<?php echo esc_html__( 'Prayer vigil information', 'cm_translate' ); ?>:
+										<?php echo esc_html( $post_meta["prayervigilinformation"][0] ); ?>
                                     </div>
 								<?php } ?>
 								<?php if ( $post_meta["greetinginformation"][0] ) { ?>
                                     <div class="deceased-greetings">
-										<?php _e( 'Greeting information', 'cm_translate' ); ?>:
-										<?php echo $post_meta["greetinginformation"][0]; ?>
+										<?php echo esc_html__( 'Greeting information', 'cm_translate' ); ?>:
+										<?php echo esc_html( $post_meta["greetinginformation"][0] ); ?>
                                     </div>
 								<?php } ?>
-                                <input type="button" onclick="location.href='<?php the_permalink(); ?>?comments'"
-                                       value="<?php _e( 'Condole', 'cm_translate' ); ?>">
+                                <input type="button"
+                                       onclick="location.href='<?php the_permalink(); ?>?comments'"
+                                       value="<?php echo esc_attr__( 'Condole', 'cm_translate' ); ?>"
+                                />
 								<?php
 								if ( $post_meta["flowers"][0] ) {
 									if ( isset( $post_meta["flowers"][0] ) ) {
@@ -151,38 +142,33 @@ get_header(); ?>
 									if ( $string != '0' ) { ?>
                                         <input type="button"
                                                onclick="location.href='<?php the_permalink(); ?>?cm-products&cm-order-form'"
-                                               value="<?php _e( 'Flowers', 'cm_translate' ); ?>">
+                                               value="<?php echo esc_attr__( 'Flowers', 'cm_translate' ); ?>"
+                                        />
 									<?php }
 								}
 
 								if ( $post_meta['coffee_table'][0] == 'yes' ) {
 									?>
-                                    <input type="button" onclick="location.href='<?php the_permalink(); ?>?ct_form'"
-                                           value="<?php _e( 'Coffee Table', 'cm_translate' ); ?>">
+                                    <input type="button"
+                                           onclick="location.href='<?php the_permalink(); ?>?ct_form'"
+                                           value="<?php echo esc_attr__( 'Coffee Table', 'cm_translate' ); ?>"
+                                    />
 									<?php
 								}
 
 								if ( $post_meta["masscard"][0] ) { ?>
-                                    <a target="_blank" href="<?php echo $post_meta["masscard"][0]; ?>">
-                                        <input type="button" value="<?php _e( 'Mass card', 'cm_translate' ); ?>"/>
+                                    <a target="_blank" href="<?php echo esc_attr( $post_meta["masscard"][0] ); ?>">
+                                        <input type="button"
+                                               value="<?php echo esc_attr__( 'Mass card', 'cm_translate' ); ?>"/>
                                     </a>
 									<?php
 								}
-
-								/* TODO: should we show this in the archive?
-								if (isset($values['live_stream'][0]) && $values['live_stream'][0] && isset($values['live_stream_url'][0]) && $values['live_stream_url'][0]) { ?>
-									<a target="_blank" href="<?= $values['live_stream_url'][0] ?>">
-										<input type="button" value="<?php _e('Funeral live-stream', 'cm_translate'); ?>" />
-									</a>
-									<?php
-								}
-								*/
 								?>
                             </div>
 
                         </div>
                         <footer class="entry-meta">
-							<?php edit_post_link( __( 'Edit', 'cm-translation' ), '<span class="edit-link">', '</span>' ); ?>
+							<?php edit_post_link( esc_html__( 'Edit', 'cm-translation' ), '<span class="edit-link">', '</span>' ); ?>
                         </footer><!-- .entry-meta -->
                     </article>
                 </div> <!-- #content -->
