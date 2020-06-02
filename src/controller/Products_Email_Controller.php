8bc08bc8a4d7a96ac3932856553c5bed9f4f780c
david <david@appsaloon.be>
2020-06-02 11:39:01 +0200

    feat(Products_Email_Controller): added ribbon text, company info, and missing translations for order email

--- src/controller/Products_Email_Controller.php
+++ src/controller/Products_Email_Controller.php
@@ -11,19 +11,24 @@ class Products_Email_Controller {
 
 	public function send_email_notification_to_wp_admin( Order $order, int $post_ID, WP_Post $post, bool $update ) {
 		if ( $update == false ) { // only send email for new orders
-			$post_meta          = get_post_meta( $post_ID );
+			$post_meta = get_post_meta( $post_ID );
+			error_log( var_export( [ "post_meta" => $post_meta ], 1 ) );
 			$condolence_exists  = isset( $post_meta['cm_order_deceased_id'][0] ) && $post_meta['cm_order_deceased_id'][0];
 			$order_lines_exists = isset( $post_meta['cm_order_order_lines'][0] ) && $post_meta['cm_order_order_lines'][0];
 			if ( $condolence_exists && $order_lines_exists ) {
 				$condolence_post_ID        = $post_meta['cm_order_deceased_id'][0];
 				$condolence_post           = get_post( $condolence_post_ID );
 				$order_lines               = json_decode( $post_meta['cm_order_order_lines'][0] );
+				$ribbon_text               = $post_meta['cm_order_ribbon_text'][0];
 				$order_contact_name        = $post_meta['cm_order_contact_first_name'][0] . " " . $post_meta['cm_order_contact_last_name'][0];
 				$order_address_line        = $post_meta['cm_order_address_line'][0];
 				$order_address_postal_code = $post_meta['cm_order_address_postal_code'][0];
 				$order_address_city        = $post_meta['cm_order_address_city'][0];
 				$order_contact_email       = $post_meta['cm_order_contact_email'][0];
 				$order_contact_phone       = $post_meta['cm_order_contact_phone'][0];
+				$order_company_name        = $post_meta['cm_order_company_name'][0];
+				$order_company_vat         = $post_meta['cm_order_company_vat'][0];
+				$order_remarks             = $post_meta['cm_order_remarks'][0];
 				$order_href                = htmlspecialchars_decode( get_edit_post_link( $post_ID ) );
 				$to                        = get_option( 'admin_email' );
 				$subject                   = sprintf( esc_html__( 'A new order has been placed for the funeral of %s', 'cm_translate' ), $condolence_post->post_title );
--- templates/new_order_email_to_site_admin.php
+++ templates/new_order_email_to_site_admin.php
@@ -1,19 +1,22 @@
 <h3><?php echo $subject; ?></h3>
 <p>
-    <?php echo esc_html__( 'The following products were ordered:', 'cm_translate' ) ?>
+    <strong><?php echo esc_html__( 'The following products were ordered', 'cm_translate' ) ?>:</strong>
 </p>
 <dl>
-    <dt><?php echo esc_html__( 'Description:', 'cm_translate' ); ?></dt>
+    <dt><?php echo esc_html__( 'Description', 'cm_translate' ); ?>:</dt>
     <dd><?php echo esc_html( $order_lines->description ) ?></dd>
-    <dt><?php echo esc_html__( 'Quantity:', 'cm_translate' ); ?></dt>
+    <dt><?php echo esc_html__( 'Quantity', 'cm_translate' ); ?>:</dt>
     <dd><?php echo esc_html( $order_lines->qty ) ?></dd>
-    <dt><?php echo esc_html__( 'Price per unit:', 'cm_translate' ); ?></dt>
+    <dt><?php echo esc_html__( 'Price per unit', 'cm_translate' ); ?>:</dt>
     <dd><?php echo esc_html( $order_lines->price->amount ) ?><?php echo esc_html( $order_lines->price->currency ) ?></dd>
-    <dt><?php echo esc_html__( 'Total price:', 'cm_translate' ); ?></dt>
+    <dt><?php echo esc_html__( 'Total price', 'cm_translate' ); ?>:</dt>
     <dd><?php echo esc_html( (int) $order_lines->price->amount * (int) $order_lines->qty ) ?><?php echo esc_html( $order_lines->price->currency ) ?></dd>
+    <dt><?php echo esc_html__( 'Ribbon text', 'cm_translate' ); ?>:</dt>
+    <dd><pre><?php echo esc_html( $ribbon_text ); ?></pre></dd>
 </dl>
+<hr/>
 <p>
-	<?php echo esc_html__( 'This order has been placed by:', 'cm_translate' ) ?>
+	<strong><?php echo esc_html__( 'This order has been placed by', 'cm_translate' ) ?>:</strong>
 </p>
 <dl>
     <dt><?php echo esc_html__( 'Name', 'cm_translate' ); ?></dt>
@@ -27,8 +30,15 @@
     <dd><?php echo esc_html( $order_contact_email ); ?></dd>
     <dt><?php echo esc_html__( 'telephone', 'cm_translate' ); ?></dt>
     <dd><?php echo esc_html( $order_contact_phone ); ?></dd>
+    <dt><?php echo esc_html__( 'Company name', 'cm_translate' ); ?>:</dt>
+    <dd><?php echo esc_html( $order_company_name ); ?></dd>
+    <dt><?php echo esc_html__( 'Company Vat', 'cm_translate' ); ?>:</dt>
+    <dd><?php echo esc_html( $order_company_vat ); ?></dd>
+    <dt><?php echo esc_html__( 'Remarks', 'cm_translate' ); ?>:</dt>
+    <dd><pre><?php echo esc_html( $order_remarks ); ?></pre></dd>
 </dl>
+<hr/>
 <p>
-	<?php echo esc_html__( 'You can view the order here:', 'cm_translate' ); ?><br/>
+	<?php echo esc_html__( 'You can view the order here', 'cm_translate' ); ?>:<br/>
     <a href="<?php echo esc_attr( $order_href ); ?>"><?php echo esc_attr( $order_href ); ?></a>
 </p>
