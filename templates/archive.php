65ca7d4485af09b1b8d29eab871df998f258f233
david <david@appsaloon.be>
2020-12-29 19:06:34 +0100

    fix(archive.php): fixed pagination when archive is rendered by a shortcode

--- templates/archive.php
+++ templates/archive.php
@@ -3,6 +3,7 @@
  * The template for displaying Archive pages.
  */
 
+use appsaloon\cm\register\Custom_Post_Type;
 use appsaloon\cm\settings\Admin_Options_Page;
 
 get_header(); ?>
@@ -21,15 +22,29 @@ get_header(); ?>
 		?>
 
 		<?php
+		global $paged;
 		global $wp_query;
 
-		$big        = 999999999; // need an unlikely integer
+		$posts_per_page = get_option( 'posts_per_page' );
+		$post_query     = new WP_Query(
+			array(
+				'showposts' => $posts_per_page,
+				'post_type' => Custom_Post_Type::post_type(),
+				'paged'     => $paged,
+			)
+		);
+
+		$total_found_posts = $post_query->found_posts;
+		$total_page        = ceil( $total_found_posts / $posts_per_page );
+
+		$big = 999999999; // need an unlikely integer
+		var_dump( $paged );
 		$pagination = paginate_links(
 			array(
 				'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 				'format'    => '?paged=%#%',
-				'current'   => max( 1, get_query_var( 'paged' ) ),
-				'total'     => $wp_query->max_num_pages,
+				'current'   => max( 1, $paged ),
+				'total'     => $total_page,
 				'next_text' => false,
 				'prev_text' => false,
 			)
@@ -47,9 +62,9 @@ get_header(); ?>
 			</div>
 		</nav>
 		<?php
-		if ( have_posts() ) :
-			while ( have_posts() ) :
-				the_post();
+		if ( $post_query->have_posts() ) :
+			while ( $post_query->have_posts() ) :
+				$post_query->the_post();
 				$post_meta = get_post_meta( get_the_ID() );
 				$is_single = false;
 				require 'deceased.php';
@@ -71,5 +86,5 @@ get_header(); ?>
 		</nav>
 	</div>
 </div>
-
+<?php wp_reset_postdata(); ?>
 <?php get_footer(); ?>
